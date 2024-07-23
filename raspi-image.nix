6530fbe52f2228abd85faf59cbc040a1a8001ffb
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-07-23 15:07:14 +0200

    small fix for open ssh and kernel.

--- raspi-image.nix
+++ raspi-image.nix
@@ -7,11 +7,12 @@
   services.sshd.enable = true;
   users.users.root.password = "nixos"; # secrets.raspi.ssh.password;
 
-  services.openssh.permitRootLogin = lib.mkDefault "yes";
+  #services.openssh.permitRootLogin = lib.mkDefault "yes";
+  services.openssh.settings.PermitRootLogin = lib.mkDefault "yes";
   services.getty.autologinUser = lib.mkDefault "root";
 
   # use the latest Linux kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Needed for https://github.com/NixOS/nixpkgs/issues/58959
   boot.supportedFilesystems = lib.mkForce [ "btrfs" ];
