928df19d5447176b0c8020b612f8ab261dbe76ed
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-07-24 17:31:55 +0200

    New attemp of building a rapsi 2 nixos image from a flake.

--- flake.nix
+++ flake.nix
@@ -1,11 +1,18 @@
 {
-  description = "Test flake to build minimum raspeberry image."
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-	nixos-hardware.url = "github:nixos/nixos-hardware?ref=latest-kernel";
+  description = "Build image";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+  outputs = { self, nixpkgs }: rec {
+    nixosConfigurations.rpi2 = nixpkgs.lib.nixosSystem {
+      system = "armv7l-linux";
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-raspberrypi.nix"
+        {
+          nixpkgs.config.allowUnsupportedSystem = true;
+          nixpkgs.crossSystem.system = "armv7l-linux";
+          # ... extra configs as above
+        }
+      ];
+    };
+    images.rpi2 = nixosConfigurations.rpi2.config.system.build.sdImage;
   };
-
-  outputs = { nixpkgs, nixos-hardware }:
-  {};
 }
