b0895f44ad3f84c8f82238ed515fbd77bc69307f
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-07-26 14:24:18 +0200

    Another try but this time for an x86 laptop nixos installer image.

--- flake.nix
+++ flake.nix
@@ -1,18 +1,18 @@
 {
-  description = "Build image";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-  outputs = { self, nixpkgs }: rec {
-    nixosConfigurations.rpi2 = nixpkgs.lib.nixosSystem {
-      system = "armv7l-linux";
-      modules = [
-        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-raspberrypi.nix"
-        {
-          nixpkgs.config.allowUnsupportedSystem = true;
-          nixpkgs.crossSystem.system = "armv7l-linux";
-          # ... extra configs as above
-        }
-      ];
-    };
-    images.rpi2 = nixosConfigurations.rpi2.config.system.build.sdImage;
+  description = "Test flake to build minimum raspeberry image.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	nixos-hardware.url = "github:nixos/nixos-hardware?ref=kernel-latest";
+	#nixos-generators.url = "";
+  };
+
+  outputs = { self, nixpkgs, nixos-hardware }:
+  {
+    laptop2 = "todo";
+
+    packages.x86_64-linux.default = (nixpkgs.lib.nixosSystem {
+	  modules = [];
+	});
   };
 }
