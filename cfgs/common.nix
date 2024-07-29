df172c53f754a63b2b5e16a51bc9a098d6727ebf
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-07-29 18:03:57 +0200

    Added future commented out setting to disable zfs (and kick out *-no-zfs.nix module) my self in the future.

--- cfgs/common.nix
+++ cfgs/common.nix
@@ -4,6 +4,7 @@
 	#nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 	boot.supportedFilesystems = [ "ext4" ];
+	#boot.supportedFilesystems.zfs = lib.mkForce false; # some day, *-no-zfs.nix installer might be obsolete
 
 	environment.systemPackages = with pkgs; [
 		git
