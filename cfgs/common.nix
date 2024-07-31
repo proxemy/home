8bd85ad2ee3161911220a72ece6fe636da8d2a19
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-07-31 17:35:23 +0200

    Extended nix image configuration. Still commented out.

--- cfgs/common.nix
+++ cfgs/common.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 {
 	# this seems to be an unknown property and breaks the iso with unpopulated /mnt-root in stage 1
-	#nix.settings.experimental-features = [ "nix-command" "flakes" ];
+	#nix.settings.system-features = [ "nix-command" "flakes" "big-parallel" "kvm" ];
 
 	boot.supportedFilesystems = [ "ext4" ];
 	#boot.supportedFilesystems.zfs = lib.mkForce false; # some day, *-no-zfs.nix installer might be obsolete
