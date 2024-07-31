e641c8e31cf6b202319290a38e319158a2c54152
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-07-31 17:44:56 +0200

    Added partition.sh script to prepare HDDs.:w

--- cfgs/common.nix
+++ cfgs/common.nix
@@ -4,21 +4,49 @@
 	#nix.settings.system-features = [ "nix-command" "flakes" "big-parallel" "kvm" ];
 
 	boot.supportedFilesystems = [ "ext4" ];
-	#boot.supportedFilesystems.zfs = lib.mkForce false; # some day, *-no-zfs.nix installer might be obsolete
 
 	environment.systemPackages = with pkgs; [
 		git
 		tmux
 		neovim
 	];
+
 	
 	# TODO move this to specialized laptop2.nix
 	isoImage = {
+		edition = lib.mkForce "laptop2";
 		isoBaseName = "laptop2-nixos";
 		volumeID = "laptop2-nixos";
 		contents = [
-			{ source = (pkgs.writeTextDir "test.sh" "echo TEST") + "/test.sh";
-			  target = "test.sh";
+			{ source = pkgs.writeText "partition.sh" ''
+#!/usr/bin/bash
+set -xeuo pipefail
+
+DEV="$1"
+
+sudo partx --show "$DEV"
+
+echo "Partitioning disc: '$DEV'! All data will be lost!"
+read -p "Proceed? [y/N]:" confirm
+[[ "$confirm" =~ y|Y ]] || { echo "Arborting"; exit 0; }
+
+sudo wipefs "$DEV"
+sudo parted -s "$DEV" mklabel msdos
+
+sudo parted -s "$DEV" -- mkpart primary 1MB -16GB
+sudo parted -s "$DEV" -- set 1 boot on
+sudo parted -s "$DEV" -- mkpart primary linux-swap -16GB 100%
+
+sudo mkfs.ext4 -L nixos "$DEV"1
+sudo mkswap -L swap "$DEV"2
+
+sudo mount /dev/disk/by-label/nixos /mnt
+sudo swapon /dev/disk/by-label/swap
+
+sudo nixos-generate-config --root /mnt
+sudo nixos-install
+'';
+			  target = "partition.sh";
 			}
 		];
 	};
