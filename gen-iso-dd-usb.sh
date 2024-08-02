f0e5ad5711b6122dfad0ad74efdbde1bf4394793
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-08-02 21:14:22 +0200

    Little polish of dd script.

--- gen-iso-dd-usb.sh
+++ gen-iso-dd-usb.sh
@@ -2,16 +2,13 @@
 
 set -xeuo pipefail
 
-# nix-shell -p nixos-generators --run "nixos-generate --format iso --configuration ./raspi-image.nix -o result"
-
-nixos-generate --flake .#laptop2 --format iso --out-link result
+nixos-generate --show-trace --flake .#laptop2 --format iso --out-link result
 ISO="result/iso/laptop2-nixos-24.11.20240725.5ad6a14-x86_64-linux.iso"
 test -f "$ISO"
+test -b /dev/sda
 
 sudo umount /dev/sda* || true
 sudo dd if="$ISO" of=/dev/sda status=progress
 sync
 sudo umount /dev/sda*
 sudo rm /dev/sda*
-# sync
-
