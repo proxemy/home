d3a155d61b97d50cd4d3acadf7dc3e4153ba31be
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-08-04 15:37:30 +0200

    Somehow, a formatter got git-hooked and reworte the entire flake...

--- flake.nix
+++ flake.nix
@@ -3,34 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-	#nixos-hardware.url = "github:nixos/nixos-hardware?ref=kernel-latest";
+    #nixos-hardware.url = "github:nixos/nixos-hardware?ref=kernel-latest";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
+    {
 
-    nixosConfigurations = {
-		laptop2 = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [
-				"${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
-				"${nixpkgs}/nixos/modules/profiles/clone-config.nix"
-				# TODO reactivate hardened kernel and fix missing ext4 support in live-nixos
-				#"${nixpkgs}/nixos/modules/profiles/hardened.nix"
-				./nix/common.nix
-			];
-		};
-	};
+      nixosConfigurations = {
+        laptop2 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
+            "${nixpkgs}/nixos/modules/profiles/clone-config.nix"
+            # TODO reactivate hardened kernel and fix missing ext4 support in live-nixos
+            #"${nixpkgs}/nixos/modules/profiles/hardened.nix"
+            ./nix/common.nix
+          ];
+        };
+      };
 
-	# naive shell for quick progress
-	# TODO: make it 'system' aware
-	devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-		buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
-			nixos-generators
-			nixos-install-tools
-			nixos-option
-			nixos-shell
-			nixos-anywhere
-		];
-	};
-  };
+      # naive shell for quick progress
+      # TODO: make it 'system' aware
+      devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
+          nixos-generators
+          nixos-install-tools
+          nixos-option
+          nixos-shell
+          nixos-anywhere
+        ];
+      };
+    };
 }
