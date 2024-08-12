64be96011e0193238a6d37e4dd7a568d7b3fd0c3
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-08-12 10:27:19 +0200

    Added mtr to global packages. Little cleanup.

--- nix/common.nix
+++ nix/common.nix
@@ -2,19 +2,19 @@
 {
 	# https://www.tweag.io/blog/2020-07-31-nixos-flakes/
 	# "required to have nix beta flake support"
-	#nix.package = pkgs.nixUnstable; # nixUnstable ist deprecated, use nixVersions.{latest,git}
 	nix.package = pkgs.nixVersions.latest;
 
 	nix.settings.system-features = [ "nix-command" "flakes" "big-parallel" "kvm" ];
 	nix.extraOptions = "experimental-features = nix-command flakes";
 
+	# TODO: move these packages in a user context, not global installation.
 	environment.systemPackages = with pkgs; [
 		git
 		tmux
 		neovim
+		mtr
 	];
 
-	# not supported by flakes. will copy /etc/nixos/configuration.nix into iso
 	system = {
 		stateVersion = "24.11";
 		autoUpgrade = {
