4720545dc9dd7669e9f9a55b3e7a21623e1cae90
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-08-12 15:31:01 +0200

    Small tidying of nix.* settings.

--- nix/common.nix
+++ nix/common.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }:
 {
-	# https://www.tweag.io/blog/2020-07-31-nixos-flakes/
-	# "required to have nix beta flake support"
-	nix.package = pkgs.nixVersions.latest;
+	nix = {
+		# https://www.tweag.io/blog/2020-07-31-nixos-flakes/
+		# "required to have nix beta flake support"
+		package = pkgs.nixVersions.latest;
 
-	nix.settings.system-features = [ "nix-command" "flakes" "big-parallel" "kvm" ];
-	nix.extraOptions = "experimental-features = nix-command flakes";
+		settings.system-features = [ "nix-command" "flakes" "big-parallel" "kvm" ];
+	};
 
 	# TODO: move these packages in a user context, not global installation.
 	environment.systemPackages = with pkgs; [
