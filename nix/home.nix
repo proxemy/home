747101efc01906ae51d981d8113b7b91f1499afe
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-08-16 15:24:46 +0200

    Small refinement of home-manager cfg.

--- nix/home.nix
+++ nix/home.nix
@@ -5,9 +5,13 @@
 	home-manager.useGlobalPkgs = true;
 	home-manager.useUserPackages = true;
 
-	home-manager.users.leme.home = {
-		inherit (config.system) stateVersion;
-		homeDirectory = "/home/leme";
-		username = "leme";
+	home-manager.users.leme = {
+		home = {
+			inherit (config.system) stateVersion;
+		};
+		programs.git = {
+			enable = true;
+			#extraConfig.include = { path = "~/.config/gitconfig/proxemy" };
+		};
 	};
 }
