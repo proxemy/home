30e377db3de0f33ae6264888d0ad685a669195bd
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-09-07 18:44:54 +0200

    Fixed conflicting boot settings with sd-card image profile

--- nix/system/rpi1/default.nix
+++ nix/system/rpi1/default.nix
@@ -6,22 +6,4 @@
   ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
-
-  boot = {
-    supportedFilesystems = [ "ext4" ];
-    loader.grub = {
-      device = "/dev/sda";
-      enable = true;
-    };
-  };
-
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
-  };
-
-  swapDevices = [ { label = "swap"; } ];
 }
