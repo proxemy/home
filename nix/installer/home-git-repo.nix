5b2bb26d953cdfc3ced9720d022144979f943a4f
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-09-10 21:42:22 +0200

    Fixed wrong executable pathes in home-git-repo creation

--- nix/installer/home-git-repo.nix
+++ nix/installer/home-git-repo.nix
@@ -14,21 +14,21 @@ pkgs.stdenv.mkDerivation {
   dontUnpack = true;
 
   buildPhase = with pkgs; ''
-    ${git}/bin init --initial-branch=main $out
+    ${git}/bin/git init --initial-branch=main $out
     cd $out
 
-    ${git-crypt}/bin unlock ${secrets.git-crypt.key-file}
+    ${git-crypt}/bin/git-crypt unlock ${secrets.git-crypt.key-file}
 
-    ${git}/bin remote add origin "https://github.com/proxemy/home"
-    ${git}/bin remote set-url --push origin git@github.com:proxemy/home.git
+    ${git}/bin/git remote add origin "https://github.com/proxemy/home"
+    ${git}/bin/git remote set-url --push origin git@github.com:proxemy/home.git
 
     # since the initial main branch is empty, we cannot set upstream the usual way
-    ${git}/bin config branch.main.remote origin
-    ${git}/bin config branch.main.merge refs/heads/main
+    ${git}/bin/git config branch.main.remote origin
+    ${git}/bin/git config branch.main.merge refs/heads/main
 
-    ${git}/bin config include.path ~/.config/gitconfig/proxemy
+    ${git}/bin/git config include.path ~/.config/gitconfig/proxemy
 
     cp --recursive ${sourceInfo}/. $out
-    ${git}/bin add . # required, so nixos-install can access tracked files
+    ${git}/bin/git add . # required, so nixos-install can access tracked files
   '';
 }
