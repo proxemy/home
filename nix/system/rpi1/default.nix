978ac2230ba0b10f1e66b6632461a74f1d9b23dd
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-09-10 17:31:07 +0200

    Second try of crosscompilation raspi image.

--- nix/system/rpi1/default.nix
+++ nix/system/rpi1/default.nix
@@ -1,10 +1,19 @@
-{ modulesPath, ... }:
+{ lib, modulesPath, ... }:
 {
   imports = [
     #"${modulesPath}/installer/sd-card/sd-image-aarch64-new-kernel-no-zfs-installer.nix"
-    "${modulesPath}/installer/sd-card/sd-image-armv7l-multiplatform-installer.nix"
+    #"${modulesPath}/installer/sd-card/sd-image-armv7l-multiplatform-installer.nix"
+    "${modulesPath}/installer/sd-card/sd-image-armv7l-multiplatform.nix"
+    #"${modulesPath}/installer/sd-card/sd-image-raspberrypi-installer.nix"
     ./../common.nix
   ];
 
-  #nixpkgs.hostPlatform = "armv7l-linux";
+  boot.supportedFilesystems.zfs = lib.mkForce false;
+
+  # 'scudo' the hardened allocator, loaded from common.nix, is incompatible with armv7l
+  environment.memoryAllocator.provider = lib.mkForce "libc";
+
+  nixpkgs.hostPlatform = "armv7l-linux";
+
+  sdImage.compressImage = false;
 }
