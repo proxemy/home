cd35ce81a435263f6ffc43056e3ed9b064ba3d37
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-09-19 10:12:23 +0200

    Fix. Raspi boots! New build mechanism (see shellHook) in combination with 'raspberrypi.nix' profile for uboot was the fix.

--- nix/system/rpi1/default.nix
+++ nix/system/rpi1/default.nix
@@ -1,10 +1,13 @@
 { lib, modulesPath, cfg, ... }:
 {
   imports = [
-    "${modulesPath}/installer/sd-card/sd-image-aarch64-new-kernel-no-zfs-installer.nix"
+    "${modulesPath}/installer/sd-card/sd-image-aarch64-new-kernel.nix"
+    "${modulesPath}/system/boot/loader/raspberrypi/raspberrypi.nix"
     ./../aarch64-cross-compilation-fixes.nix
     ./../common.nix
   ];
+
+  boot.loader.raspberryPi.version = 4;
 
   nixpkgs = {
     buildPlatform = "x86_64-linux";
