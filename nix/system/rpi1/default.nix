1727d4e8abb95191622c2850cdca58e262fbd1c7
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-09-20 09:58:14 +0200

    Fixed missing swap file (work in progress).

--- nix/system/rpi1/default.nix
+++ nix/system/rpi1/default.nix
@@ -5,7 +5,16 @@
     "${modulesPath}/system/boot/loader/raspberrypi/raspberrypi.nix"
     ./../aarch64-cross-compilation-fixes.nix
     ./../common.nix
-  ];
+  ]; # TODO exclude cross compilation fixes for native builds so native auto updates
+  #  can pick up the formerly excluded features
+  # requires passed down builtins.currentSystem, which is flake-impure ...
+  /*
+    +++ (
+      if stdenv.buildPlatform.system != stdenv.hostPlatform.system
+      then []
+      else []
+    );
+  */
 
   boot.loader.raspberryPi.version = 4;
 
@@ -15,4 +24,18 @@
   };
 
   sdImage.compressImage = false;
+
+  # TODO Disabled for testing, since 'swapDevice' might fully create a swapfile
+  /*sdImage.postBuildCommands = ''
+    dd if=/dev/zero of="$out"/swapfile bs=1024 count=$((1024*1024*8))
+    chmod 0600 "$out"/swapfile
+    mkswap "$out"/swapfile
+  '';*/
+
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 8 * 1024;
+    }
+  ];
 }
