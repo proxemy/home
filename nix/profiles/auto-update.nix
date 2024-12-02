70def8846e47b6fee904c238386da178dca599c7
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-12-02 13:18:24 +0100

    Fixed dysfunctional git-crypt and systemd service execution order.

--- nix/profiles/auto-update.nix
+++ nix/profiles/auto-update.nix
@@ -23,24 +23,29 @@
     };
   };
 
-  # nixos-upgrade.service
   systemd.services.home-git-repo-update = {
     description = "Pull from remote and update lockfile. See HOMEDIR";
+    before = [ "nixos-upgrade.service" ];
     requiredBy = [ "nixos-upgrade.service" ];
     serviceConfig = {
       Type = "oneshot";
       WorkingDirectory = cfg.homeDir;
-      UMask="0077";
+      UMask = "0077";
+      Environment = "PATH=$PATH:${pkgs.git-crypt}/bin";
       ExecStart =
+        with pkgs;
         let
           #git_args = "--work-tree=${cfg.homeDir} --git-dir=${cfg.homeDir}/.git";
-          nix_args = "--extra-experimental-features nix-command";
+          #nix_args = "--extra-experimental-features nix-command --extra-experimental-features falkes";
         in
-        with pkgs; [
-          "${git}/bin/git fetch origin"
-          "${git}/bin/git reset --hard origin/main"
-          "${git-crypt}/bin/git-crypt unlock ${cfg.homeDir}/.git/git-crypt/keys/default"
-          "${nix}/bin/nix ${nix_args} flake update"
+        [
+          "${git}/bin/git checkout main --quiet"
+          "${git}/bin/git reset --hard --quiet"
+          "${git}/bin/git pull origin main --quiet"
+          "${git}/bin/git branch --verbose"
+          "${git-crypt}/bin/git-crypt unlock .git/git-crypt/keys/default"
+          # TODO enable flake.lock upgrade after some testing of failure conditions
+          #"${nix}/bin/nix ${nix_args} flake update"
         ];
     };
   };
