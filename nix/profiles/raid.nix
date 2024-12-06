26de5cb388a736635160418fd5bc9e6c951db0d3
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-12-06 13:02:03 +0100

    First working setup of RAID

--- nix/profiles/raid.nix
+++ nix/profiles/raid.nix
@@ -1,7 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  mdadm_event_handler = pkgs.writeShellScript "mdadm_event_handler.sh" ''
+    #!{pkgs.bash}/bin/bash
+    echo $* | systemd-cat --identifier=mdadm --priority=alert
+  '';
+in
 {
-  # TODO: either mdadm, zfs or btrfs
-  boot.swraid = {
-    #enable = true;
+  boot = {
+    swraid = {
+      enable = true;
+      mdadmConf = ''
+        ARRAY /dev/md/0 level=raid1 num-devices=3 metadata=1.2 UUID=c07219cf:4e1bca0c:8d3e245f:204b8044
+        devices=/dev/sda,/dev/sdb,/dev/sdc
+        PROGRAM ${mdadm_event_handler}
+      '';
+    };
+
+    kernelModules = [ "md_mod" "raid1" ];
   };
 }
