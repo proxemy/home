652556029aeeb6c1be4aa75e74dc4dcb65794750
Proxemy <33573115+proxemy@users.noreply.github.com>
2025-01-25 17:12:24 +0100

    Updated 'flake.lock'. 'raspberry.nix' got deprecated.

--- nix/system/rpi1/default.nix
+++ nix/system/rpi1/default.nix
@@ -2,14 +2,11 @@
 {
   imports = [
     "${modulesPath}/installer/sd-card/sd-image-aarch64-new-kernel.nix"
-    "${modulesPath}/system/boot/loader/raspberrypi/raspberrypi.nix"
     ./../../profiles/aarch64-cross-compilation-fixes.nix
     ./../../profiles/common.nix
     ./../../services/nas_server.nix
     ./../../services/raid.nix
   ]; # TODO exclude cross compilation fixes for native builds so native auto updates
-
-  boot.loader.raspberryPi.version = 4;
 
   sdImage.compressImage = false;
 
