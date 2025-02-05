d2c22f39596a25c90554c95d466e97377ae618a5
Proxemy <33573115+proxemy@users.noreply.github.com>
2025-02-05 15:29:26 +0100

    formatting

--- nix/lib/mkSystemdService.nix
+++ nix/lib/mkSystemdService.nix
@@ -2,22 +2,37 @@
 # TODO
 {
   description,
+
+  # parameters passed to ServiceConfig
+  PrivateTmp ? "yes",
+  NoNewPrivileges ? true,
+  ProtectSystem ? "strict",
+  CapabilityBoundingSet ? "CAP_NET_BIND_SERVICE CAP_DAC_READ_SEARCH",
+  RestrictNamespaces ? "uts ipc pid user cgroup net mnt",
+  ProtectKernelTunables ? "yes",
+  ProtectKernelModules ? "yes",
+  ProtectControlGroups ? "yes",
+  PrivateDevices ? "yes",
+  RestrictSUIDSGID ? true,
+  IPAddressAllow,
 }:
 {
   inherit description;
 
   # see: https://github.com/alegrey91/systemd-service-hardening
   serviceConfig = {
-    PrivateTmp = "yes";
-    NoNewPrivileges = true;
-    ProtectSystem = "strict";
-    CapabilityBoundingSet = "CAP_NET_BIND_SERVICE CAP_DAC_READ_SEARCH";
-    RestrictNamespaces = "uts ipc pid user cgroup";
-    ProtectKernelTunables = "yes";
-    ProtectKernelModules = "yes";
-    ProtectControlGroups = "yes";
-    PrivateDevices = "yes";
-    RestrictSUIDSGID = true;
-    #IPAddressAllow = "192.168.1.0/24";
+    inherit
+      PrivateTmp
+      NoNewPrivileges
+      ProtectSystem
+      CapabilityBoundingSet
+      RestrictNamespaces
+      ProtectKernelTunables
+      ProtectKernelModules
+      ProtectControlGroups
+      PrivateDevices
+      RestrictSUIDSGID
+      IPAddressAllow
+      ;
   };
 }
