ef49a8beebf90e3abe4c61184036d9e44992b267
Proxemy <33573115+proxemy@users.noreply.github.com>
2025-10-16 15:09:25 +0200

    First step towards generalized partion script. Created hosts partitions definitions for laptop1.

--- nix/system/installer/script.nix
+++ nix/system/installer/script.nix
@@ -5,35 +5,58 @@
 }:
 let
   inherit (cfg) home_git_dir;
+
+  partx = "${pkgs.util-linux}/bin/partx";
+  wipefs = "${pkgs.util-linux}/bin/wipefs";
+  parted = "${pkgs.parted}/bin/parted";
+  mkfs = "${pkgs.util-linux}/bin/mkfs";
+  mkswap = "${pkgs.util-linux}/bin/mkswap";
+
+  test_confirm_wipe = device: ''
+    sudo -s
+    test -b "${device}" || { echo Target device "${device}" is no block device; exit 1; }
+    ${partx} --show ${device}
+    echo All data will be lost!
+    [[ $(read -p "Proceed? [y|N]:"; echo $REPLY) =~ y|Y ]] || exit 0
+    ${wipefs} -a "${device}"
+  '';
+
+  mk_primary = partition:
+  let
+    device = "/dev/disk/by-id/${partition.id}";
+  in
+  ''
+    ${test_confirm_wipe device}
+    ${parted} -s "${device}" mklabel ${partition.label}
+    ${parted} -s "${device}" -- mkpart primary 1MB -${partition.swap_size}GB
+    ${parted} -s "${device}" -- set 1 boot on
+    ${parted} -s "${device}" -- mkpart primary linux-swap -${partition.swap_size}GB 100%
+    ${mkfs} -t ${partition.fs} -L nixos "${device}-part1"
+    ${mkswap} -L swap "${device}-part2"
+  '';
+
+  mk_secondary = partition:
+  let
+    device = "/dev/disk/by-id/${partition.id}";
+  in
+  ''
+    echo TODO
+  '';
 in
 pkgs.writeShellScript "install.sh" ''
-  set -euo pipefail
+  set -xeuo pipefail
 
-  test -b '${host.root_fs.device}' || { echo Illegal target device: ${host.root_fs.device}; exit 1; }
-
-  ${pkgs.util-linux.bin}/bin/partx --show '${host.root_fs.device}'
-
-  echo Partitioning disc: '${host.root_fs.device}'! All data will be lost!
-  read -p "Proceed? [y/N]:" confirm
-  [[ "$confirm" =~ y|Y ]] || { echo "Arborting"; exit 0; }
-
-  set -x
-
-  ${pkgs.util-linux.bin}/bin/wipefs '${host.root_fs.device}'
-
-  ${host.root_fs.partition_script pkgs}
-
-  set +x
+  ${mk_primary host.partitions.primary}
 
   # the host.root_fs.parition script needs to create 2 labeled partitions, nixos and swap
   ${pkgs.util-linux.bin}/bin/mount /dev/disk/by-label/nixos /mnt
   ${pkgs.util-linux.bin}/bin/swapon /dev/disk/by-label/swap
 
-  mkdir -p "\mnt\${home_git_dir}"
-  cp -r /iso/home-git/. "\mnt\${home_git_dir}"
+  mkdir -p "/mnt/${home_git_dir}"
+  cp -r /iso/home-git/. "/mnt/${home_git_dir}"
 
   nixos-install \
-  --flake "\mnt\${home_git_dir}"#${host.hostname} \
+  --flake "/mnt/${home_git_dir}"#${host.hostname} \
   --root /mnt \
   --no-root-passwd \
   --verbose \
