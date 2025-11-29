f47f50b7c579a204d21f8a02e3787f408c21b7a4
Proxemy <33573115+proxemy@users.noreply.github.com>
2025-11-29 11:16:19 +0100

    fmt

--- nix/programs/keepassxc.nix
+++ nix/programs/keepassxc.nix
@@ -4,15 +4,16 @@
     pkgs.keepassxc
   ];
 
-  /* TODO
-  security.apparmor.policies.keepassxc = {
-    state = "complain";
-    profile = ''
-      abi <abi/4.0>
-      ${pkgs.keepassxc}/bin/keepassxc {
-        /home/${secrets.user_name}/.config/keepassxc/** r,
-      }
-    '';
-  };
+  /*
+    TODO
+    security.apparmor.policies.keepassxc = {
+      state = "complain";
+      profile = ''
+        abi <abi/4.0>
+        ${pkgs.keepassxc}/bin/keepassxc {
+          /home/${secrets.user_name}/.config/keepassxc/** r,
+        }
+      '';
+    };
   */
 }
