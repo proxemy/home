ede0b19b40f525abaeef8be0eb4fd67c20c293a9
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-02-06 11:43:14 +0100

    More xfce settings. workspaces=1, prevent_focus_stealing, urgent_blink, panel-2/autohide, pulseaudio plugin, alt-tab.

--- nix/profiles/xfce.nix
+++ nix/profiles/xfce.nix
@@ -1,4 +1,9 @@
-{ secrets, config, ... }:
+{
+  config,
+  pkgs,
+  secrets,
+  ...
+}:
 let
   gtk_color_theme = "Adwaita-dark";
 in
@@ -18,22 +23,32 @@ in
     displayManager.defaultSession = "xfce";
   };
 
-  #
-  # XFCE config files are stored in ~/.config/xfce4/xfconf
-  # xfconf-query -c CHANNEL (-l) -p PROPERTY
-  # xfconf-query -c xfce4-desktop -p "/last/window-height"
-  #
+  # TODO: maybe move this to a dedicated profile/sound.nix
+  environment.systemPackages =
+    if config.services.pulseaudio.enable then
+      [
+        pkgs.xfce4-pulseaudio-plugin
+      ]
+    else
+      [ ];
 
   home-manager.users.${secrets.user_name} = {
+    #
+    # XFCE config files are stored in ~/.config/xfce4/xfconf
+    # xfconf-query -c CHANNEL (-l) -p PROPERTY
+    # xfconf-query -c xfce4-desktop -p "/last/window-height"
+    #
     xfconf = {
       enable = true;
       settings = {
+
         xfce4-desktop = {
           # Disable all but workplace0
           "backdrop/screen0/monitorsLVDS-1/workplace0/color-style" = 0;
           "backdrop/screen0/monitorsLVDS-1/workplace0/image-style" = 0;
           "backdrop/screen0/monitorsLVDS-1/workplace0/last-image" = "";
         };
+
         xfce4-keyboard-shortcuts = {
           "commands/custom/override" = true;
           "commands/custom/<Primary><Alt>Delete" = "xfce4-session-logout";
@@ -46,6 +61,7 @@ in
           #"commands/custom/<Super>r/startup-notify" = true;
 
           "xfwm4/custom/override" = true;
+          "xfwm4/custom/<Alt>Tab" = "cycle_windows_key";
           "xfwm4/custom/<Super>Up" = "maximize_window_key";
           "xfwm4/custom/<Super>Down" = "hide_window_key";
           "xfwm4/custom/<Super>Left" = "move_window_left_key";
@@ -53,18 +69,29 @@ in
           "xfwm4/custom/<Super><Shift>End" = "move_window_next_workspace_key";
           "xfwm4/custom/<Super><Shift>Home" = "move_window_prev_workspace_key";
         };
+
         xfce4-panel = {
           "panels/dark-mode" = true;
+          # Disable bottom panel by any means
+          "panels/panel-2/autohide-behavior" = 2;
         };
+
         xsettings = {
           "Net/ThemeName" = gtk_color_theme;
         };
+
         xfwm4 = {
           # window decorations theme
           "general/theme" = "BBS";
 
           # Disable compositor as a workaround for latest nvidia driver flickering
           "general/use_compositing" = if config.hardware.nvidia.enabled then false else true;
+
+          "general/workspace_count" = 1;
+          "general/scroll_workspaces" = false;
+          "general/prevent_focus_stealing" = true;
+          "general/urgent_blink" = true;
+          "general/repeat_urgent_blink" = true;
         };
       };
     };
