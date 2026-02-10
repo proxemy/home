81056791762268a0f48e4752cab141c6de51dc3f
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-02-10 15:48:00 +0100

    Reworked akregator feeds import.

--- nix/programs/akregator.nix
+++ nix/programs/akregator.nix
@@ -13,11 +13,18 @@
             pkgs.kdePackages.akregator
           ];
 
-          file.akregator_feeds = {
-            target = ".local/share/akregator/data/feeds.opml";
-            text = secrets.feeds.OPML;
-            force = true;
-          };
+          file.akregator_feeds =
+            let
+              source = builtins.toFile "feeds.opml" secrets.feeds.OPML;
+              target_dir = ".local/share/akregator/data/feeds.opml";
+            in
+            {
+              inherit source;
+              onChange = ''
+                chmod 0077
+                cp ${source} ~/${target_dir}
+              '';
+            };
         };
       }
     else
