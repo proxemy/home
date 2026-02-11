f9658ac9f3fcdcc35218cdcb40a33ebfe3864b48
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-02-11 12:29:05 +0100

    Added akregator config file.

--- nix/programs/akregator.nix
+++ nix/programs/akregator.nix
@@ -19,12 +19,59 @@
               target_dir = ".local/share/akregator/data/feeds.opml";
             in
             {
+              # akregator writes to the feeds.opml, copying it then.
               inherit source;
               onChange = ''
                 chmod 0077
                 cp ${source} ~/${target_dir}
               '';
             };
+        };
+
+        xdg.configFile."akregatorrc" = {
+          force = true;
+          text = ''
+            [Advanced]
+            Mark Read Delay=1
+
+            [Appearance]
+            Fixed Font=monospace
+            Sans Serif Font=Sans
+            Serif Font=Sans
+            Standard Font=Sans
+
+            [Archive]
+            ArchiveMode=limitArticleNumber
+
+            [Browser]
+            EnableJavascript=false
+            LMB Behaviour=OpenInExternalBrowser
+
+            [General]
+            ArticleListFeedHeaders=AAAA/wAAAAAAAAABAAAAAQAAAAMBAAAAAAAAAAAAAAAEAgAAAAEAAAABAAAAZAAABY0AAAAEAQEAAAAAAAAAAAAAAAAAAGT/////AAAAgQAAAAAAAAAEAAAEswAAAAEAAAAAAAAAAAAAAAEAAAAAAAAAZAAAAAEAAAAAAAAAdgAAAAEAAAAAAAAD6AAAAABkAAAAAAAAAAAAAAAAAAAAAQ==
+            ArticleListGroupHeaders=AAAA/wAAAAAAAAABAAAAAQAAAAMBAAAAAAAAAAAAAAAAAAAAAAAABiYAAAAEAQEAAAAAAAAAAAAAAAAAAGT/////AAAAgQAAAAAAAAAEAAAE6AAAAAEAAAAAAAAAZAAAAAEAAAAAAAAAZAAAAAEAAAAAAAAAdgAAAAEAAAAAAAAD6AD/////AAAAAAAAAAAAAAAAAAAAAQ==
+            PreviousNewFeaturesMD5=rMGXcQMwFu7xghlAkh4bXg==
+            Show Tray Icon=false
+            Show Unread In Taskbar=false
+            SubscriptionListHeaders=AAAA/wAAAAAAAAABAAAAAQAAAAAAAAAAAAAAAAAAAAADBgAAAAIAAAABAAAAHAAAAAIAAABkAAABrAAAAAMBAAABAAAAAAAAAAAAAAAAZP////8AAACBAAAAAAAAAAMAAAGsAAAAAQAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAQAAAAAAAAPoAAAAAGQAAAAAAAAAAAAAAAAAAAAB
+            Use Interval Fetch=false
+
+            [HTML Settings]
+            AccessKeyEnabled=false
+            Fonts=Sans,monospace,Sans,Sans,Sans,Sans,0
+            MediumFontSize=14
+            MinimumFontSize=12
+
+            [Network]
+            Use HTML Cache=false
+
+            [Security]
+            LoadExternalReferences=false
+
+            [View]
+            Splitter 1 Sizes=430,1486
+            Splitter 2 Sizes=321,630
+          '';
         };
       }
     else
