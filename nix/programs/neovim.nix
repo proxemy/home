b51dd7c3eeffe54613bf7698b52e48dba54ccbe8
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-02-13 15:39:36 +0100

    Added plugins and treesitter parsers.

--- nix/programs/neovim.nix
+++ nix/programs/neovim.nix
@@ -1,10 +1,43 @@
-{ pkgs, ... }:
+{ pkgs, secrets, ... }:
 {
-  programs = {
-    neovim.enable = true;
+  home-manager.users.${secrets.username}.programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+
+    plugins =
+      with pkgs.vimPlugins;
+      [
+        syntastic
+        nvim-treesitter
+        YouCompleteMe
+      ]
+      ++ (with pkgs.vimPlugins.nvim-treesitter-parsers; [
+        bash
+        c
+        c_sharp
+        cpp
+        dockerfile
+        git_config
+        git_rebase
+        gitattributes
+        gitcommit
+        gitignore
+        html
+        java
+        latex
+        lua
+        markdown
+        nix
+        python
+        regex
+        rust
+        sql
+        vim
+        vimdoc
+      ]);
   };
 
-  # neovim/plugins dependencies
+  # dependencies
   environment = {
     systemPackages = with pkgs; [
       gcc
