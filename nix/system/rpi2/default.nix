ef44c99163848889987666579043e9b0ab700d70
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-03-04 10:08:52 +0100

    Fixed immediate build problems with rpi2.

--- nix/system/rpi2/default.nix
+++ nix/system/rpi2/default.nix
@@ -1,12 +1,18 @@
 { modulesPath, ... }:
 {
   imports = [
-    "${modulesPath}/installer/sd-card/sd-image-aarch64-new-kernel.nix"
+    "${modulesPath}/installer/sd-card/sd-image-armv7l-multiplatform.nix"
     ./../../profiles/aarch64-cross-compilation-fixes.nix
     ./../../profiles/headless.nix
     ./../../profiles/common.nix
     ./../../services/tor.nix
   ];
+
+  # "scudo" from hardened profile is not available for armv7l-linux
+  environment.memoryAllocator.provider = "jemalloc";
+
+  # pkgs.efivar is broken in armv7l-linux
+  nixpkgs.config.allowBroken = true;
 
   sdImage.compressImage = false;
 
