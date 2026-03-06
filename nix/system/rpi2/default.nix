f37d27ad537664cb494880219a29c5060bca089c
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-03-06 18:05:56 +0100

    Found a better way to disable core modlues and disabled tor service for now.

--- nix/system/rpi2/default.nix
+++ nix/system/rpi2/default.nix
@@ -1,27 +1,33 @@
 {
-  pkgs,
   lib,
   modulesPath,
-  config,
   ...
 }:
 let
-  # remove import of
-  helpers = import ../../lib/helpers.nix { inherit pkgs lib config; };
-  armv7_image = helpers.strip_profile_imports "${modulesPath}/installer/sd-card/sd-image-armv7l-multiplatform.nix";
+  # for initial emulated compilation, enable temporarily
+  # ! DO NOT COMMIT / PUSH !
+  is_cross = false;
+  build_platform = "x86_64-linux";
 in
 {
   imports = [
-    armv7_image
-    "${modulesPath}/installer/sd-card/sd-image.nix"
+    "${modulesPath}/installer/sd-card/sd-image-armv7l-multiplatform.nix"
     ./../../profiles/headless.nix
     ./../../profiles/common.nix
-    ./../../services/tor.nix
+    #./../../services/tor.nix
   ];
 
-  # for initial emulated compilation, enable temporarily
-  # ! DO NOT COMMIT / PUSH !
-  #nixpkgs.buildPlatform = "x86_64-linux";
+  # cross compilation fixes
+  disabledModules = [
+    # pkgs.efivar is imported by base.nix but broken for armv7l
+    "${modulesPath}/profiles/base.nix"
+  ]
+  ++ (lib.optionals is_cross [
+    ../../services/auto-update.nix
+    ../../profiles/cli_minimal.nix
+  ]);
+
+  nixpkgs.buildPlatform = if is_cross then build_platform else null;
 
   # "scudo" from hardened profile is not available for armv7l-linux
   environment.memoryAllocator.provider = "jemalloc";
