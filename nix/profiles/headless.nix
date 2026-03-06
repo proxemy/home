328da5f2fe2ed246675b8da7fbd737260f22a5e7
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-03-06 13:46:01 +0100

    Extended profiles/headless.nix

--- nix/profiles/headless.nix
+++ nix/profiles/headless.nix
@@ -1,8 +1,12 @@
-{ modulesPath, ... }:
+{ lib, modulesPath, ... }:
 {
   imports = [
     "${modulesPath}/profiles/minimal.nix"
+    # TODO enable and test this
+    #"${modulesPath}/profiles/headless.nix"
   ];
+
+  environment.defaultPackages = lib.mkForce [ ];
 
   console.enable = false;
 }
