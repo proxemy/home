7557371f4b1b4b7393f039aafaa45a812543cb35
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-03-10 18:08:43 +0100

    enabled tor relay, firewall bypass and local client, rate limit and tor-status alias.

--- nix/services/tor.nix
+++ nix/services/tor.nix
@@ -2,10 +2,33 @@
 {
   services.tor = {
     enable = true;
+    openFirewall = true;
+    client.enable = true;
 
-    relay.role = [
-      "bridge"
-      "relay"
-    ];
+    relay = {
+      enable = true;
+      role = "bridge";
+    };
+
+    # https://2019.www.torproject.org/docs/tor-manual.html.en
+    # https://github.com/torproject/manual
+    # https://tb-manual.torproject.org/
+    # http://dsbqrprgkqqifztta6h3w7i2htjhnq7d3qkh3c7gvc35e66rrcv66did.onion/
+    settings = rec {
+      BandWidthRate = "1 MBytes";
+      RelayBandwidthRate = BandWidthRate;
+      #ConnLimit = 200;
+      #ExtraInfo = "1";
+      ConnDirectionStatistics = true;
+    };
+  };
+
+  environment.shellAliases = {
+    tor-status = ''
+      systemctl status tor.service --no-pager
+      #cat /var/lib/tor/fingerprint
+      #cat /var/lib/tor/stats/*
+    '';
+
   };
 }
