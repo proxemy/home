efc191c410ddb5b552c27151c73b298347f8de0e
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-03-10 18:05:53 +0100

    Added disabled core module nixos/modules/profiles/base.nix

--- nix/profiles/headless.nix
+++ nix/profiles/headless.nix
@@ -6,6 +6,11 @@
     #"${modulesPath}/profiles/headless.nix"
   ];
 
+  disabledModules = [
+    # imported by sd-image profiles for raspis
+    "${modulesPath}/profiles/base.nix"
+  ];
+
   environment.defaultPackages = lib.mkForce [ ];
 
   console.enable = false;
