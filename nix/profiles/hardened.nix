2f92195c7e91387deacf9b91b7f1efc3f0c9e93c
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-03-24 13:51:19 +0100

    Disabled sysctl workaround for failing logrotate compilation.

--- nix/profiles/hardened.nix
+++ nix/profiles/hardened.nix
@@ -14,7 +14,5 @@
 
   # TMP: workaround for logrotate, a common dependency, failing to build
   # https://discourse.nixos.org/t/logrotate-config-fails-due-to-missing-group-30000
-  boot.kernel.sysctl = {
-    "kernel.unprivileged_userns_clone" = 1;
-  };
+  #boot.kernel.sysctl."kernel.unprivileged_userns_clone" = 1;
 }
