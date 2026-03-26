ac9de64457f65d116adc5cf0b17f4a6577092b06
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-03-26 10:25:54 +0100

    Put raid commands into sudoers

--- services/raid.nix
+++ services/raid.nix
@@ -1,12 +1,16 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  secrets,
+  ...
+}:
 let
   # TODO test and refine this maybe
   mdadm_event_handler = pkgs.writeShellScript "mdadm_event_handler.sh" ''
-    #!{pkgs.bash}/bin/bash
-    echo $* | systemd-cat --identifier=mdadm --priority=alert
+    echo "$*" | systemd-cat --identifier=mdadm --priority=alert
   '';
 
-  raid = rec {
+  raid = {
     level = "raid1";
     num-devices = "3";
     auto = "no";
@@ -29,11 +33,51 @@ let
   };
 
   systemd_service_names = {
-    automout =
+    automount =
       builtins.replaceStrings [ "/" ] [ "-" ] (builtins.substring 1 99 mount.target) + ".automount";
     nfs-server = "nfs-server.service";
   };
   systemd_service_names_list = builtins.attrValues systemd_service_names;
+
+  # sudo commands for shell aliases and sudoers file
+  cmds =
+    with lib;
+    let
+      mdadm = "${getBin pkgs.mdadm}/bin/mdadm";
+      cryptsetup = "${getBin pkgs.cryptsetup}/bin/cryptsetup";
+      systemctl = "${getBin pkgs.systemd}/bin/systemctl";
+      umount = "${getBin pkgs.util-linux}/bin/umount";
+    in
+    {
+      mdadm_detail = "${mdadm} --detail ${mount.source}";
+      mdadm_stop = "${mdadm} --stop --verbose ${mount.source}";
+      mdadm_asssemble =
+        "${mdadm} --assemble --verbose ${mount.source} "
+        + builtins.toString (
+          builtins.map (num: "/dev/mapper/luks" + num) [
+            "1"
+            "2"
+            "3"
+          ]
+        );
+
+      cryptsetup_open_1 = "${cryptsetup} luksOpen /dev/sda luks1";
+      cryptsetup_open_2 = "${cryptsetup} luksOpen /dev/sdb luks2";
+      cryptsetup_open_3 = "${cryptsetup} luksOpen /dev/sdc luks3";
+      cryptsetup_close =
+        "${cryptsetup} luksClose "
+        + builtins.toString [
+          "luks1"
+          "luks2"
+          "luks3"
+        ];
+
+      systemctl_stop = "${systemctl} stop ${builtins.toString systemd_service_names_list}";
+      systemctl_start = "${systemctl} start ${builtins.toString systemd_service_names_list}";
+
+      umount = "${umount} ${mount.source}";
+    };
+
 in
 {
   boot = {
@@ -85,32 +129,28 @@ in
       #dmsetup ls --tree -o blkdevname,uuid
       raid-status = ''
         lsblk -s ${mount.source} -o NAME,SIZE,FSTYPE,FSVER,LABEL,FSAVAIL,FSUSED,MOUNTPOINTS
-        mdadm --detail ${mount.source}
+        sudo ${cmds.mdadm_detail}
         cat /proc/mdstat
       '';
 
       # TODO maybe chain it together in systemd units and remove nfs stuff here
       raid-stop = ''
-        systemctl stop ${builtins.toString systemd_service_names_list}
-        umount ${mount.source}
-        mdadm --stop --verbose ${mount.source}
+        sudo ${cmds.systemctl_stop}
+        sudo ${cmds.umount}
+        sudo ${cmds.mdadm_stop}
       '';
 
       raid-start = ''
-        mdadm --assemble --verbose ${mount.source} /dev/mapper/luks{1,2,3}
-        systemctl start ${builtins.toString systemd_service_names_list}
+        sudo ${cmds.mdadm_asssemble}
+        sudo ${cmds.systemctl_start}
       '';
 
-      raid-lock = ''
-        cryptsetup luksClose luks1
-        cryptsetup luksClose luks2
-        cryptsetup luksClose luks3
-      '';
+      raid-lock = "sudo ${cmds.cryptsetup_close}";
 
       raid-unlock = ''
-        cryptsetup luksOpen /dev/sda luks1
-        cryptsetup luksOpen /dev/sdb luks2
-        cryptsetup luksOpen /dev/sdc luks3
+        sudo ${cmds.cryptsetup_open_1}
+        sudo ${cmds.cryptsetup_open_2}
+        sudo ${cmds.cryptsetup_open_3}
       '';
 
       raid-help = ''
@@ -118,24 +158,35 @@ in
         --- format:
         cryptsetup luksFormat --debug --type luks2 --integrity hmac-sha256 /dev/sd-
         --- create:
-        mdadm --create --verbose --level 1 --raid-devices=4 /dev/md0 [ /dev/mapper/luks1 <mapped devices> ]
+        mdadm --create --verbose --level 1 --raid-devices=3 /dev/md0 [ /dev/mapper/luks1 <mapped devices> ]
         --- partition & mount:
         mkfs.ext4 -v /dev/md0 && mount /dev/md0 /mnt/raid
         --- Re-add missing drives:
         mdadm --re-add /dev/md0 /dev/mapper/luks1
-        --- TODO: how to integrate new devices
         "
       '';
 
       raid-up = ''
         raid-unlock
         raid-start
+        raid-status
       '';
 
       raid-down = ''
         raid-stop
         raid-lock
+        raid-status
       '';
     };
   };
+
+  security.sudo.extraRules = [
+    {
+      users = [ secrets.username ];
+      commands = builtins.map (cmd: {
+        command = cmd;
+        options = [ "NOPASSWD" ];
+      }) (builtins.attrValues cmds);
+    }
+  ];
 }
