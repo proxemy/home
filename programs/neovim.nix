ba71c2b48a41b504b5ea2c5a33cbe0d5a0cf18d8
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-03-27 21:15:49 +0100

    Replaced YouCompleteMe with coc.

--- programs/neovim.nix
+++ programs/neovim.nix
@@ -8,7 +8,11 @@
   home-manager.users.${secrets.username}.programs.neovim = {
     enable = true;
     defaultEditor = true;
-    withPython3 = true; # required by YouCompleteMe
+    #withPython3 = true; # required by YouCompleteMe
+    coc = {
+      enable = true;
+      package = pkgs.vimPlugins.coc-nvim;
+    };
 
     plugins =
       with pkgs.vimPlugins;
@@ -20,7 +24,7 @@
         # Supported Languages: Ruby, Lua, Vimscript, Bash, Elixir, Fish, Julia
         #nvim-treesitter-endwise
         rustaceanvim
-        YouCompleteMe
+        #YouCompleteMe
       ]
 
       ++ (with pkgs.vimPlugins.nvim-treesitter-parsers; [
@@ -46,6 +50,23 @@
         sql
         vim
         vimdoc
+      ])
+
+      ++ (with pkgs.vimPlugins; [
+        coc-clangd
+        coc-cmake
+        coc-git
+        coc-html
+        coc-java
+        coc-json
+        coc-markdownlint
+        coc-nvim
+        coc-pyright
+        coc-rust-analyzer
+        coc-sh
+        coc-toml
+        coc-vimlsp
+        coc-vimtex
       ]);
 
     initLua = ''
@@ -53,27 +74,12 @@
 
         -- https://github.com/nvim-treesitter/nvim-treesitter-context/
         require'treesitter-context'.setup{
-          enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-          multiwindow = false, -- Enable multiwindow support.
-          max_lines = 2, -- How many lines the window should span. Values <= 0 mean no limit.
-          min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+          enable = true,
+          multiwindow = false,
+          max_lines = 2,
           line_numbers = true,
-          multiline_threshold = 20, -- Maximum number of lines to show for a single context
-          trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-          mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
-          -- Separator between context and content. Should be a single character string, like '-'.
-          -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-          separator = nil,
-          zindex = 20, -- The Z-index of the context window
-          on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+          trim_scope = 'outer',
       }
     '';
   };
-
-  # dependencies
-  users.users.${secrets.username}.packages = with pkgs; [
-    #gcc
-    #python3
-    #tree-sitter
-  ];
 }
