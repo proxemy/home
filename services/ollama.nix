6c8cb7d9026ae8477b9f4daec87dd8aa0de56264
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-04-23 11:58:05 +0200

    Changed ollama package to vulkan, removed codellama and deepseek and added qwen3.6.

--- services/ollama.nix
+++ services/ollama.nix
@@ -1,7 +1,8 @@
 { pkgs, lib, ... }:
 let
   user = "ollama";
-  pkg = pkgs.ollama-rocm;
+  #pkg = pkgs.ollama-rocm;
+  pkg = pkgs.ollama-vulkan;
 in
 
 {
@@ -12,15 +13,17 @@ in
     package = pkg;
 
     loadModels = [
-      "gemma3:27b"
-      "deepseek-v3"
-      "codellama:70b"
+      #"codellama:70b"
+      #"deepseek-v3"
+      "gemma4:31b"
+      "qwen3.6:35n"
     ];
   };
 
+  # TODO: fix: ollama-model-loader.service cannot download models for a confined ollama.service
   systemd.services.ollama = {
     enable = true;
-    confinement.enable = true;
+    confinement.enable = false;
     wantedBy = lib.mkForce [ ];
   };
 }
