d8675276547bc116679a7f4b49c9584b9bddae69
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-05-15 12:50:40 +0200

    Added steam guard systemd service.

--- programs/steam.nix
+++ programs/steam.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # https://nixos.wiki/wiki/Steam
   programs.steam.enable = true;
@@ -8,6 +8,43 @@
     "steam-unwrapped"
   ];
 
-  # disabled by hardening profile but steam relies on it
-  boot.kernel.sysctl."kernel.unprivileged_userns_clone" = 1;
+  systemd =
+    let
+      name = "steam-guard";
+      steam = "/run/current-system/sw/bin/steam";
+    in
+    {
+
+      timers.${name} = {
+        description = "${name}.timer triggers ${name}.service periodically.";
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnBootSec = 0;
+          OnCalendar = "Mon..Fri *-*-* 00..18:*";
+        };
+      };
+
+      services.${name} = {
+        description = "Kills steam and make it non-executable.";
+        enableStrictShellChecks = true;
+        after = [ "multi-user.target" ];
+
+        serviceConfig = {
+          Type = "oneshot";
+          LogLevelMax = "emerg";
+        };
+
+        script = with pkgs; ''
+          ${coreutils}/bin/chmod -x ${steam} 2&>/dev/null || true
+          if pid="$(${procps}/bin/pidof steam)"; then
+            ${procps}/bin/kill "$pid"
+          fi
+          ${coreutils}/bin/sleep 58
+        '';
+
+        postStop = with pkgs; ''
+          ${coreutils}/bin/chmod +x ${steam} 2&>/dev/null || true
+        '';
+      };
+    };
 }
