9b865f3d7f0109cbdd80a81937234b53dea334c7
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-05-22 17:57:35 +0200

    Logging + reenabled apparmor

--- profiles/hardened.nix
+++ profiles/hardened.nix
@@ -1,5 +1,6 @@
 {
   modulesPath,
+  secrets,
   ...
 }:
 {
@@ -9,8 +10,16 @@
 
   security = {
     #auditd.enable = true;
-    #apparmor.enable = true;
+    apparmor.enable = true;
   };
+
+  networking.firewall = {
+    logRefusedConnections = true;
+    logRefusedPackets = true;
+    logReversePathDrops = true;
+  };
+
+  nix.settings.allowed-users = [ secrets.username ];
 
   # TMP: workaround for logrotate, a common dependency, failing to build
   # https://discourse.nixos.org/t/logrotate-config-fails-due-to-missing-group-30000
