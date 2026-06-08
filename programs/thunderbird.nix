ffe2eb88ca503b6e57ed2f7a1f6032d446546dea
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-06-08 20:44:08 +0200

    Added thunderbird socks proxy.

--- programs/thunderbird.nix
+++ programs/thunderbird.nix
@@ -1,4 +1,4 @@
-{ secrets, ... }:
+{ secrets, self, ... }:
 {
   home-manager.users.${secrets.username} = {
     programs.thunderbird = {
@@ -7,17 +7,27 @@
       profiles.default = {
         isDefault = true;
 
-        settings = {
-          "mail.spellcheck.inline" = false;
-          "extensions.activeThemeID" = "thunderbird-compact-dark@mozilla.org";
+        settings =
+          let
+            socks_ip = secrets.hosts.rpi2.ip;
+            socks_port =
+              self.nixosConfigurations.${secrets.hotnames.rpi2}.config.services.tor.client.socksListenAddress.port;
+          in
+          {
+            "mail.spellcheck.inline" = false;
+            "extensions.activeThemeID" = "thunderbird-compact-dark@mozilla.org";
 
-          "javascript.enabled" = false;
-          "network.protocol-handler.warn-external.http" = true;
-          "network.protocol-handler.warn-external.https" = true;
+            "javascript.enabled" = false;
+            "network.protocol-handler.warn-external.http" = true;
+            "network.protocol-handler.warn-external.https" = true;
 
-          "mailnews.display.prefer_plaintext" = true;
-          "plain_text.wrap_long_lines" = false;
-        };
+            "mailnews.display.prefer_plaintext" = true;
+            "plain_text.wrap_long_lines" = false;
+
+            "network.proxy.socks" = socks_ip;
+            "network.proxy.socks_port" = socks_port;
+            "network.proxy.allow_bypass" = false;
+          };
       };
     };
   };
