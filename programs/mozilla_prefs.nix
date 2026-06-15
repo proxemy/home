ce0a0c48c8729d9f5f93fc27e6218573166e84d4
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-06-15 13:35:47 +0200

    missed some AI disablings.

--- programs/mozilla_prefs.nix
+++ programs/mozilla_prefs.nix
@@ -5,15 +5,22 @@
   disable_ai ? true,
 }:
 lib.optionalAttrs disable_ai {
-  "browser.ml.enable" = false;
   "browser.ml.chat.enabled" = false;
-  "browser.ml.pageAssist.enabled" = false;
+  "browser.ml.chat.sidebar" = false;
+  "browser.ml.chat.menu" = false;
+  "browser.ml.chat.page" = false;
+  "browser.ml.enable" = false;
   "browser.ml.linkPreview.enabled" = false;
-  "extensions.ml.enabled" = false;
-  "browser.tabs.groups.smart.enabled" = false;
+  "browser.ml.pageAssist.enabled" = false;
+  "browser.ml.smartAssist.enabled" = false;
   "browser.search.visualSearch.featureGate" = false;
+  "browser.tabs.groups.smart.enabled" = false;
+  "browser.tabs.groups.smart.userEnabled" = false;
+  "extensions.ml.enabled" = false;
   "pdfjs.enableAltText" = false;
   "sidebar.revamp" = false; # optional
+  "pdfjs.enableAltTextModelDownload" = false;
+  "pdfjs.enableGuessAltText" = false;
 
   "browser.ai.control.default" = "blocked";
   "browser.ai.control.linkPreviewKeyPoints" = "blocked";
