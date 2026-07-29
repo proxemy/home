2f61f8f07ca6012c537fbcbe0701f83c2348d403
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-07-29 13:30:43 +0200

    Little less redshift

--- programs/redshift.nix
+++ programs/redshift.nix
@@ -9,7 +9,7 @@
     xdg.configFile."redshift.conf".text = ''
       [redshift]
       temp-day=5700
-      temp-night=2400
+      temp-night=3400
       gamma-day=0.8
       gamma-night=0.4
       adjustment-method=randr
