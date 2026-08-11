9026827c7e01a266fcbc7e7909c1a6e83cb8d83d
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-08-11 20:38:51 +0200

    Added hardening and privacy chromium switches.

--- programs/chromium.nix
+++ programs/chromium.nix
@@ -1,8 +1,35 @@
-{ pkgs, secrets, ... }:
+{
+  pkgs,
+  lib,
+  cfg,
+  secrets,
+  ...
+}:
 {
   home-manager.users.${secrets.username}.programs.chromium = {
     enable = true;
     package = (pkgs.chromium.override { enableWideVine = true; });
+
+    commandLineArgs = [
+      "--block-new-web-contents"
+      "--disable-background-mode"
+      "--disable-background-networking"
+      "--disable-client-side-phishing-detection"
+      "--disable-component-update"
+      "--disable-default-apps"
+      "--disable-domain-reliability"
+      #"--disable-features=Prerender2"
+      "--disable-file-system"
+      "--incognito"
+      "--isolation-by-default"
+      "--no-pings"
+      "--process-per-site"
+      "--site-per-process"
+    ]
+    ++ lib.optionals cfg.debug [
+      "--enable-logging=stderr"
+      "--v=2"
+    ];
   };
 
   nixpkgs.config.allowUnfreePackages = [
