66cb615e282d9e57a25063aff72605347904b665
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-08-21 21:31:54 +0200

    Enabled goose apparmor profile.

--- programs/goose.nix
+++ programs/goose.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   home-manager,
   config,
   secrets,
@@ -22,9 +23,38 @@ let
     ]
   );
 
-  xdg = config.home-manager.users.${secrets.username}.xdg;
-  home = config.users.users.${secrets.username}.home;
+  hm_user = config.home-manager.users.${secrets.username};
+  xdg = hm_user.xdg;
+  home = hm_user.home.homeDirectory;
   goose_yaml = "${xdg.configHome}/goose/config.yaml";
+
+  dependencies =
+    with pkgs;
+    [
+      glibc
+      gcc-unwrapped
+      libgcc
+      readline
+      ncurses
+      acl
+      attr
+
+      # from: https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/config/vte.nix
+      (pkgs.vte.override {
+        withApp = false;
+        gtkVersion = null;
+      })
+
+    ]
+    ++ goose_pkg.buildInputs;
+
+  allowed_tools = with pkgs; [
+    goose_pkg
+    bash
+    bash-completion
+    coreutils
+    coreutils-full
+  ];
 in
 
 {
@@ -44,17 +74,71 @@ in
       state = "enforce";
 
       profile = ''
+        include <tunables/global>
+
         profile ${goose_pkg}/bin/* {
           ${home}/src/** rw,
-          ${home}/**/.git/** r,
+          ${home}/**/{.git,.svn,.hg}/** r,
+          deny ${home}/.bash_history rwk,
 
-          ${xdg.binHome}/** r,
-          ${xdg.configHome}/** r,
-          ${xdg.stateHome}/goose/** rw,
+          #${xdg.binHome}/** r,
+          #${xdg.configHome}/** r,
+          ${xdg.configHome}/goose/** rwk,
+          ${xdg.dataHome}/goose/** rwk,
+          ${xdg.stateHome}/goose/** rwk,
 
-          deny network,
+          #deny network,
           deny dbus,
           deny signal,
+
+          # dependencies
+          ${builtins.foldl' (
+            acc: dep:
+            acc
+            + ''
+              ${lib.getLib dep}/** rm,
+            ''
+          ) "" dependencies}
+
+          # tools
+          ${builtins.foldl' (
+            acc: tool:
+            acc
+            + ''
+              ${lib.getBin tool}/bin/* rix,
+              ${tool}/** r,
+            ''
+          ) "" allowed_tools}
+
+          # data
+          ${config.i18n.glibcLocales}/** r,
+          ${config.environment.etc.profile.source} r,
+          ${config.environment.etc.bashrc.source} r,
+          ${config.environment.etc."profiles/per-user/${secrets.username}".source}/** r,
+          ${config.home-manager.users."${secrets.username}".home.file.".profile".source} r,
+          ${config.system.path}/** r,
+          ${hm_user.home.file.".bash_profile".source} r,
+          ${pkgs.tzdata}/** r,
+          ${pkgs.cacert}/** r,
+
+          # TODO: find package of gmp-with-cxx and remove broad /nix/store access
+          /nix/store/*-gmp-with-cxx-*/lib/** rm,
+          /nix/store/** r,
+
+          /etc/ssl/certs/ r,
+          /etc/ssl/certs/** r,
+          /etc/pki/tls/certs/ r,
+          /etc/pki/tls/certs/** r,
+          /dev/tty rw,
+          /dev/pts/** rw,
+          /dev/urandom r,
+          /dev/null rw,
+          /sys/devices/system/cpu/** r,
+          /sys/fs/cgroup/user.slice/** r,
+          /proc/stat r,
+          /proc/self/** r,
+          /proc/@{pid}/** r,
+          /tmp/** wr,
         }
       '';
     };
