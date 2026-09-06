df5f46b615142fb0b1d571e77c994de0f9001ad9
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-09-06 21:24:52 +0200

    Fall back to nix-tools stable for raspis.

--- profiles/headless.nix
+++ profiles/headless.nix
@@ -1,17 +1,24 @@
-{ lib, modulesPath, ... }:
+{
+  pkgs,
+  lib,
+  modulesPath,
+  ...
+}:
 {
   imports = [
     "${modulesPath}/profiles/minimal.nix"
-    # TODO enable and test this
-    #"${modulesPath}/profiles/headless.nix"
   ];
 
   disabledModules = [
-    # imported by sd-image profiles for raspis
+    # imported by sd-image profiles for raspis, breaks build
     "${modulesPath}/profiles/base.nix"
   ];
 
   environment.defaultPackages = lib.mkForce [ ];
 
   console.enable = false;
+
+  # headless means running on various ISAs, stable is a conservative choice.
+  # TODO: maybe create dedicated aarch64 profile
+  nix.package = lib.mkForce pkgs.nixVersions.stable;
 }
