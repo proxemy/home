2e1ec35e0464cb6b95cf7228b07664948360d7ca
Proxemy <33573115+proxemy@users.noreply.github.com>
2026-09-10 16:22:25 +0200

    Fixed missing rust-src dep for rust-analyzer

--- programs/vscodium.nix
+++ programs/vscodium.nix
@@ -11,9 +11,15 @@
     programs.vscodium = {
       enable = config.hardware.graphics.enable;
 
-      profiles.default.extensions = with pkgs.vscode-extensions; [
-        rust-lang.rust-analyzer
-      ];
+      profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+          rust-lang.rust-analyzer
+        ];
+        userSettings = {
+          # rust-analyer searches for installed rust-src
+          "rust-analyzer.cargo.sysrootSrc" = "${pkgs.rustPlatform.rustLibSrc}";
+        };
+      };
     };
   };
 }
